d4527ddaf8fc71fa03217e981fd918daf2150431
Andzhey123 <beginov.a@gmail.com>
2022-08-09 00:08:44 +0300

    Добавлено решение по задаче №6

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Программа проверки является ли число чётным");
+
+Console.WriteLine("Введите целое число:");
+
+string? numberString = Console.ReadLine();
+
+int number = int.Parse(numberString!);
+
+if (number%2==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
