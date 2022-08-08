1a73ecdf2b6cb744e80147112c5613e19f275d60
Andzhey123 <beginov.a@gmail.com>
2022-08-09 00:20:42 +0300

    Добавил решение по задаче №8

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Программа вывода всех чётных чисел от 1 до N");
+
+Console.WriteLine ("Введите число:");
+
+string? NumberString1 = Console.ReadLine ();
+
+int number1 = int.Parse (NumberString1!);
+
+Console.WriteLine ("Чётные числа от 1 до "+ number1 +":");
+
+int CountNumber = 0;
+
+while (CountNumber <= number1)
+{
+    CountNumber +=2;
+    if (CountNumber <= number1)
+    {
+       Console.Write (CountNumber + " "); 
+    }
+}
