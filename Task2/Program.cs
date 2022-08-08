81c7b42c8bdd57d798e5d181ff3ba858cd443e51
Andzhey123 <beginov.a@gmail.com>
2022-08-08 22:38:32 +0300

    Добалено решение по задаче №2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Программа нахождения максимального из двух чисел");
+
+Console.WriteLine("Введите первое число");
+
+string? numberString = Console.ReadLine();
+
+int nuberA = int.Parse(numberString!);
+
+Console.WriteLine("Введите второе число");
+
+string? numberStringA = Console.ReadLine();
+
+int nuberB = int.Parse(numberStringA!);
+
+if (nuberA>nuberB)
+{
+    Console.WriteLine(nuberA+" большее число ");
+}
+else
+{
+    Console.WriteLine(nuberB+" большее число ");
+}
