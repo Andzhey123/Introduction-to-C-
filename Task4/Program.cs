1fb0f306299bea0c2ce2988bcd043a30dbc5fd52
Andzhey123 <beginov.a@gmail.com>
2022-08-08 23:41:34 +0300

    Добавил решение задачи №4

---
+++ Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Программа нахождения максимального из трех чисел");
+
+Console.WriteLine("Введите первое число");
+
+string? numberString = Console.ReadLine();
+
+int nuberA = int.Parse(numberString!);
+
+Console.WriteLine("Введите второе число");
+
+string? numberStringA = Console.ReadLine();
+
+int nuberB = int.Parse(numberStringA!);
+
+Console.WriteLine("Введите третье число");
+
+string? numberStringB = Console.ReadLine();
+
+int nuberC = int.Parse(numberStringB!);
+
+int Max = nuberA;
+
+if (nuberB>Max)
+{
+    Max = nuberB;
+}
+if (nuberC>Max)
+{
+    Max = nuberC;
+}
+
+Console.WriteLine(Max+" большее число");
